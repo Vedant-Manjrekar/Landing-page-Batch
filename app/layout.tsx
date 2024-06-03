c24116957b67f189071124a0ca13e7fecad2558f
Vedant Manjrekar <vedant.28m@gmail.com>
2024-06-03 14:08:17 +0530

    First Version.

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import Navbar from "./components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Responsive Landing Page",
+  title: "Landing Page",
   description: "Created By Vedant M.",
 };
 
